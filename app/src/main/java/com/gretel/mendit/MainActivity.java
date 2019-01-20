46b0712262d5231fa0bd5169e8cfc7d398dae2d3
Amik Mandal <am641@duke.edu>
2019-01-20 00:41:00 -0500

    made readable

--- app/src/main/java/com/gretel/mendit/MainActivity.java
+++ app/src/main/java/com/gretel/mendit/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-import gretel.com.linex.R;
+import gretel.com.mendit.R;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/gretel/mendit/MainActivityRecyclerViewAdapter.java
+++ app/src/main/java/com/gretel/mendit/MainActivityRecyclerViewAdapter.java
@@ -14,21 +14,34 @@ import com.bumptech.glide.Glide;
 import java.util.ArrayList;
 
 import de.hdodenhof.circleimageview.CircleImageView;
-import gretel.com.linex.R;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import gretel.com.mendit.R;
 
 public class MainActivityRecyclerViewAdapter extends RecyclerView.Adapter<MainActivityRecyclerViewAdapter.ViewHolder>{
 
-    public static final String TAG = "RecyclerViewAdapter";
-
     private ArrayList<String> myProfilePhotos = new ArrayList<String>();
-    private ArrayList<String> myPartners = new ArrayList<String>();
-    private ArrayList<String> myPartnerPayments = new ArrayList<String>();
+    private ArrayList<String> myRepairerNames = new ArrayList<String>();
+    private ArrayList<String> myRatings = new ArrayList<String>();
     private Context myContext;
 
-    public MainActivityRecyclerViewAdapter(ArrayList<String> profilePhotos, ArrayList<String> partners, ArrayList partnerPayments, Context context){
+    public MainActivityRecyclerViewAdapter(ArrayList<String> profilePhotos, ArrayList<String> repairers, ArrayList ratings, Context context){
         myProfilePhotos = profilePhotos;
-        myPartners = partners;
-        myPartnerPayments = partnerPayments;
+        myRepairerNames = repairers;
+        myRatings = ratings;
         myContext = context;
     }
 
@@ -47,28 +60,27 @@ public class MainActivityRecyclerViewAdapter extends RecyclerView.Adapter<MainAc
                 .load(myProfilePhotos.get(position))
                 .into(viewHolder.profilePhoto);
 
-        viewHolder.partnerName.setText(myPartners.get(position));
-        viewHolder.partnerPaid.setText(myPartners.get(position));
+        viewHolder.repairerName.setText(myRepairerNames.get(position));
+        viewHolder.repairerRatings.setText(myRepairerNames.get(position));
     }
 
+    //not meant to be 0. To be changed to a variable based on database.
     @Override
     public int getItemCount() {
         return 0;
     }
 
-
     public class ViewHolder extends RecyclerView.ViewHolder{
-
         CircleImageView profilePhoto;
-        TextView partnerName;
-        TextView partnerPaid;
+        TextView repairerName;
+        TextView repairerRatings;
         RelativeLayout parentLayout;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            profilePhoto=itemView.findViewById(R.id.line_partner_profile_photo);
-            partnerName=itemView.findViewById(R.id.line_partner_name);
-            partnerPaid=itemView.findViewById(R.id.line_partner_text_view);
+            profilePhoto=itemView.findViewById(R.id.repairer_profile_photo);
+            repairerName=itemView.findViewById(R.id.repairer_name);
+            repairerRatings=itemView.findViewById(R.id.line_partner_text_view);
         }
     }
 }
